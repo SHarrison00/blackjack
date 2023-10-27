c11389b91e7d82201cb7a002f702ad56f93dd97e
SHarrison00 <86479780+SHarrison00@users.noreply.github.com>
2023-10-27 12:43:29 +0100

    first updates
    - deck() to build_deck() name change
    - added card_value() function

--- logic.py
+++ logic.py
@@ -1,12 +1,12 @@
 from itertools import product
 import random
 
-def deck():
+def build_deck():
     """
     Create a deck of playing cards.
 
     Returns:
-    list of tuples: A deck of playing cards.
+        list of tuples: A deck of playing cards.
     """
     suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
     ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -19,11 +19,25 @@ def shuffle(deck):
     Shuffle a deck of playing cards.
 
     Returns:
-    list of tuples: A shuffled deck of playing cards.
+        list of tuples: A shuffled deck of playing cards.
     """
     # TO DO (Conor):
 
     # Hint: 
         # How might we use the "random" module to shuffle a deck? 
         # https://docs.python.org/3/library/random.html
+    pass
+
+
+def card_value(card):
+    """
+    Returns:
+        int: Value of a single card.
+    """
+    # TO DO (Conor):
+
+    # Hint: 
+        # Consider ranks of all cards, i.e. ['2', ...,  '10', 'J', 'Q', 'K', 'A']
+        # The value for some cards are easier than others
+        # Could we use "if elif else" statements?
     pass
