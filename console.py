7b5465fd6d3577dad5816cc90410198d3441f980
SHarrison00 <86479780+SHarrison00@users.noreply.github.com>
2023-10-30 18:12:57 +0000

    functions for tomorrow's session
    - draw_initial_hand()
    - display_dealer_hand()
    - set conor the challenge of running the game in the console, see console.py

--- console.py
+++ console.py
@@ -3,3 +3,14 @@ from logic import *
 deck = build_deck()
 
 print(deck)
+
+# TO DO: Conor
+    # Challenge: Start to implement Blackjack in the console.
+
+# For example, you might want to keep start above. 
+# 
+# I think this is ok for now.
+#
+# Next, we might want to draw some cards. Then, we might want to reveal some 
+# cards in the console. Then, we might want the user to make a decision, i.e.
+# hit/stand etc. etc. etc. ...
--- logic.py
+++ logic.py
@@ -36,3 +36,88 @@ def card_value(card):
         return 11
     else:
         return int(rank)
+    
+
+def draw_card(deck):
+    """
+    Draw a single card from the deck.
+
+    Args:
+        deck (list of tuples): The deck of playing cards.
+
+    Returns:
+        tuple: A single card.
+    """
+    # TO DO: Conor
+    pass    
+
+
+def draw_initial_hand(deck):
+    """
+    Draw an initial hand of two cards from the deck.
+
+    Args:
+        deck (list of tuples): The deck of playing cards.
+
+    Returns:
+        list: A list containing two cards.
+    """
+    # TO DO: Conor
+    pass
+
+
+def display_player_hand(player_hand):
+    """
+    Display the player's hand.
+
+    Args:
+        player_hand (list of tuples): Player's hand, i.e list of card tuples.
+    """
+    # TO DO: Conor
+
+    # Example:
+    # --------
+    # Say the player's hand is
+    # player_hand = [('2', 'Hearts'), ('Ace', 'Spades')]
+    
+    # Then the output should be 
+    # display_player_hand(player_hand)
+    # >>> Player's Hand: [('2', 'Hearts'), ('Ace', 'Spades')]
+    
+    # Hint:
+        # See how to combine strings and lists using print().
+        # The solution is simple, and this is not a trick*. 
+    pass
+
+
+def display_dealer_hand(dealer_hand, reveal_hole_card=False):
+    """
+    Display the dealer's hand.
+
+    Args:
+        dealer_hand (list of tuples): Dealer's hand, i.e. list of card tuples.
+        reveal_hole_card (bool, optional): Whether to reveal the hole card.
+    """
+    # TO DO: Conor
+
+    # Example:
+    # --------
+    # Let's consider the dealer's hand with an unrevealed hole card:
+    # dealer_hand = [('5', 'Diamonds'), ('King', 'Clubs')]
+    
+    # When calling the function WITHOUT REVEALING the hole card:
+    # display_dealer_hand(dealer_hand)
+    
+    # The output should be:
+    # >>> Dealer's Hand: [('5', 'Diamonds'), ('??', '??')]
+    
+    # When calling the function WITH REVEALING the hole card revealed:
+    # display_dealer_hand(dealer_hand, reveal_hole_card=True)
+    
+    # The output should be:
+    # >>> Dealer's Hand: [('5', 'Diamonds'), ('King', 'Clubs')]
+    
+    # Hint:
+    # - Use an if-else statement to decide whether to reveal the hole card.
+    # - You can print lists directly to display the hand.
+    pass
