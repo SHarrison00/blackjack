b1cde62e423dbaaab39021460003df29e0162b7c
SHarrison00 <86479780+SHarrison00@users.noreply.github.com>
2023-11-08 13:16:04 +0000

    added hand value and user input functions

--- logic.py
+++ logic.py
@@ -38,6 +38,43 @@ def card_value(card):
         return int(rank)
     
 
+def hand_value(hand):
+    """
+    Calculate the value of a hand in Blackjack.
+
+    Args:
+        hand (list of tuples): The player's hand, i.e., a list of card tuples.
+
+    Returns:
+        int: The value of the hand.
+    """
+    # Initialize the total value of the hand
+    total_value = 0  
+    # Initialize count for 'Ace' cards in hand
+    ace_count = 0
+
+    # Calculate the value of each card in the hand using card_value function
+    for card in hand:
+        # Calculate the value of the current card using the card_value function
+        card_val = card_value(card)
+        # Update the total_value by adding the card's value
+        total_value += card_val
+        # Check if the card is an 'Ace' and update ace_count accordingly
+            # TODO: Conor ...
+
+    # In this 'while' loop:
+    # Check if there are 'Ace' cards in the hand (ace_count > 0)
+    # and if the total_value exceeds 21 (total_value > 21)
+    while ace_count > 0 and total_value > 21:
+        # If both conditions are met, adjust the value of 'Ace' cards:
+        # - Deduct 10 from total_value, i.e change the value of 'Ace' from 11 to 1
+        # - Decrement ace_count for each 'Ace' adjusted
+            # TODO: Conor ...
+        pass
+
+    return total_value
+
+    
 def draw_card(deck):
     """
     Draw a single card from the deck.
@@ -82,31 +119,32 @@ def display_dealer_hand(dealer_hand, reveal_hole_card=False):
         dealer_hand (list of tuples): Dealer's hand, i.e. list of card tuples.
         reveal_hole_card (bool, optional): Whether to reveal the hole card.
     """
-    # TO DO: Conor
-
-    # Example:
-    # --------
-    # Let's consider the dealer's hand with an unrevealed hole card:
-    # dealer_hand = [('5', 'Diamonds'), ('King', 'Clubs')]
-    
-    # When calling the function WITHOUT REVEALING the hole card:
-    # display_dealer_hand(dealer_hand)
-    
-    # The output should be:
-    # >>> Dealer's Hand: [('5', 'Diamonds'), ('??', '??')]
-    
-    # When calling the function WITH REVEALING the hole card revealed:
-    # display_dealer_hand(dealer_hand, reveal_hole_card=True)
-    
-    # The output should be:
-    # >>> Dealer's Hand: [('5', 'Diamonds'), ('King', 'Clubs')]
-    
-    # Hint:
-    # - Use an if-else statement to decide whether to reveal the hole card.
-    # - You can print lists directly to display the hand.
     if reveal_hole_card:
         print("Dealer's Hand", dealer_hand)
     else:
         concealed_card = [('??', '??')]
         visible_cards = concealed_card + dealer_hand[:1]
         print("Dealer's Hand:", visible_cards)
+
+
+def get_user_decision():
+    """
+    Get the user's decision in the game, whether to "Hit" or "Stand".
+
+    Returns:
+        str: The user's decision, either "Hit" or "Stand".
+    """
+    while True:
+        # Use input() to get user input
+        user_input = input("Do you want to 'Hit' or 'Stand'? ").strip().lower()
+
+        # Check if the user's input is valid (either "hit" or "stand")
+        if user_input == "hit":
+            # TODO: Implement the code for "Hit" choice here
+            pass
+        elif user_input == "stand":
+            # TODO: Implement the code for "Stand" choice here
+            pass
+        else:
+            # If the input is not valid, prompt the user again
+            print("Invalid input. Please enter 'Hit' or 'Stand'.")
--- test.py
+++ test.py
@@ -29,7 +29,21 @@ class TestShuffleFunction(unittest.TestCase):
     def test_ace_card_value(self):
         card = ('Diamonds', 'A')
         result = card_value(card)
-        self.assertEqual(result, 11)    
+        self.assertEqual(result, 11)
+
+    
+    def test_hand_value_no_aces(self):
+        # Test a hand with no Aces
+        hand = [('Hearts', '7'), ('Clubs', '9')]  # Total value = 7 + 9 = 16
+        result = hand_value(hand)
+        self.assertEqual(result, 16)
+
+
+    def test_hand_value_with_aces(self):
+        # Test a hand with Aces, where one Ace is counted as 11 and the other as 1
+        hand = [('Diamonds', 'A'), ('Spades', '5'), ('Hearts', 'A')]  # Total value = 11 + 5 + 1 = 17
+        result = hand_value(hand)
+        self.assertEqual(result, 17)
 
 
 if __name__ == '__main__':
